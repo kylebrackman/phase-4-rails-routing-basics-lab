d5b8f1e834af388b300c82b59af3304f987a9dac
Kyle Brackman <kyle.a.brackman@gmail.com>
2023-03-26 21:51:13 -0600

    commit tree 5a7502efee05b09d0613acc68fe7d5cb781d9dfc

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,11 @@
+class StudentsController < ApplicationController
+
+    def index
+        render json: Student.all
+    end
+
+    def grades
+        students = Student.order(grade: :desc)
+        render json: students
+    end
+end
